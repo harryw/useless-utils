8d9895c93fa71325a4d249e1e4f42ce2002c665c
Your Name <hwilkinson@mdsol.com>
2010-04-26 14:26:10 +0100

    modified assembly info

--- UselessUtils/Properties/AssemblyInfo.cs
+++ UselessUtils/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UselessUtils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Some boring utility classes")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Medidata Solutions")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UselessUtils")]
-[assembly: AssemblyCopyright("Copyright © Medidata Solutions 2010")]
+[assembly: AssemblyCopyright("Copyright © Harry Wilkinson 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
